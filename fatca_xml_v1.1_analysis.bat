7fac4f2e63f75e35049323a4e90dad1cb7ec114f
wuqing1450 <qingwu@outlook.sg>
2016-08-26 16:16:19 +0800

    Add files via upload

---
+++ fatca_xml_v1.1_analysis.bat
@@ -0,0 +1,3 @@
+
+java -jar lib/xsdvi.jar fatac_xml_v_1_1/FatcaXML_v1.1.xsd fatac_xml_v_1_1/stffatcatypes_v1.1.xsd fatac_xml_v_1_1/isofatcatypes_v1.0.xsd fatac_xml_v_1_1/oecdtypes_v4.1.xsd -generateStyle xsdvi.css
+        
---
+++ fatca_xml_v2.0_analysis.bat
@@ -0,0 +1,3 @@
+
+java -jar lib/xsdvi.jar fatac_xml_v_2_0/FatcaXML_v2.0.xsd fatac_xml_v_2_0/stffatcatypes_v2.0.xsd fatac_xml_v_2_0/isofatcatypes_v1.1.xsd fatac_xml_v_2_0/oecdtypes_v4.2.xsd -generateStyle xsdvi.css
+        
